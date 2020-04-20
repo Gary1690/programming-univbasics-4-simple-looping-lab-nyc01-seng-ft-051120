7220545ec5b93e5542106e83d307ee9ebc8d58fd
Gary Cordero Rosa <garycordero1690@gmail.com>
2020-04-20 01:42:54 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,15 +1,17 @@
 # Write your methods here
 def loop_message_five_times (message)
   counter = 0 
-  while counter > 5 do
+  while counter < 5 do
     puts message
     counter+=1 
   end
 end
 
+
+
 def loop_message_n_times (message,n)
   counter = 0 
-  while counter > n do
+  while counter < n do
     puts message
     counter+=1 
   end
